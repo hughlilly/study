8b2ad3f517dee30f9b6398aee1c41d8cfd32147d
hughlilly <hughlilly@users.noreply.github.com>
2022-03-19 12:43:57 +1300

    final version (w note on future error-handling)

--- ProgrammingPrinciples/Assessment1/IT5014_PracticalAssessment1_ 20220344/GradeCalculator.py
+++ ProgrammingPrinciples/Assessment1/IT5014_PracticalAssessment1_ 20220344/GradeCalculator.py
@@ -1,27 +1,29 @@
 '''
 Task 2: Grade calculator
 
-Takes two scores, one for a project, and one for an exam. Each contributes 50%
+Takes two scores: one for a project, and one for an exam. Each contributes 50%
 to the final grade. Grade boundaries are as shown in the following table:
 
-+--------------+-------+
-|  Percentage  | Score |
-+--------------+-------+
-| 80 or above  | A     |
-| 70 to 79     | B     |
-| 60 to 69     | C     |
-| 50 to 59     | D     |
-| 49 and below | Fail  |
-+--------------+-------+
++---------------+-------+
+|  Percentage   | Score |
++---------------+-------+
+| 80 or above   |   A   |
+| 70 to 79      |   B   |
+| 60 to 69      |   C   |
+| 50 to 59      |   D   |
+| 49 and below  | Fail  |
++---------------+-------+
 '''
 
-print("Welcome to the Grade Calculator. Please enter your grades.")
+# Print welcome message and ask for two scores; cast them to integers
+print("\nWelcome to the Grade Calculator. Please enter your grades.")
 project_score = int(input("What did you receieve on the project? "))
 exam_score = int(input("What did you receive on the exam? "))
 
+# Calculate the total grade percentage; each mark is worth 50%
 percentage = (project_score + exam_score) * .5
 
-# Work out letter grade, starting with fail (less than 50)
+# Work out corresponding letter grade, starting with Fail (less than 50)
 if percentage < 50:
     grade = "fail"
 elif percentage >= 50 and percentage < 60:
@@ -33,9 +35,14 @@ elif percentage >= 70 and percentage < 80:
 elif percentage >= 80:
     grade = "A"
 
-print("You received a " + grade + " grade.")
+# Concatenate grade into report string and print out
+print("You received a " + grade + " grade.\n")
 
 # This program fulfils the following test-case assertion:
 #
-# Given input of "78" for the project, and "65" for the exam (71.5%), the program
-# calculates a "B" grade.
+# Given input of "78" for the project, and "65" for the exam (71.5%),
+# the program calculates a "B" grade.
+
+# NB: The program assumes the user enters two positive integers; simple error-
+# handling could be added in an `else` block after the last `elif` to deal
+# with the entry of the numeral `0`, negative integers, and text strings.
