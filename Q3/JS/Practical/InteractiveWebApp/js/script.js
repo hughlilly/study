f2b84be0f3e8fb275939a348d4e63f68e22261f3
hughlilly <hughlilly@users.noreply.github.com>
2022-08-19 22:03:32 +1200

    delete function working

--- Q3/JS/Practical/InteractiveWebApp/js/script.js
+++ Q3/JS/Practical/InteractiveWebApp/js/script.js
@@ -1,22 +1,27 @@
-// Add a new item to the list
+// A: Add a new item to the list
 
-// Get form element and store in variable
+// Get form element
 const formElement = document.querySelector("form");
-
-// Get existing list element
-let existingList = document.querySelector("#to-do-list");
 
 // Listen for the "submit" event on the button
 formElement.addEventListener("submit", (event) => {
 	// Stop page from reloading
 	event.preventDefault();
 
-	// Get value from form input field, pass to function
+	// Get value from form input field
 	let newItemText = formElement.querySelector("#input-txt").value;
-	let newListElement = createNewElement(newItemText);
 
-	// Append new element to existing list
-	existingList.appendChild(newListElement);
+	// If not blank, continue with adding process
+	if (newItemText != "") {
+		// Pass input text to function and store result in variable
+		let newListElement = createNewElement(newItemText);
+
+		// Append new element to existing list
+		existingList.appendChild(newListElement);
+
+		// Reset value
+		formElement.querySelector("#input-txt").value = "";
+	}
 });
 
 //  Create a new element to append to the list, complete with
@@ -51,8 +56,23 @@ function createNewElement(newItemText) {
 	return newListElement;
 }
 
-// Listen for a click on any of the "delete" buttons
-const deleteButtons = document.querySelectorAll("del-btn");
-deleteButtons.addEventListener("click", (event) => {
-	console.log(event.target);
+// B: Delete and update list items
+
+// Get existing list element
+const existingList = document.querySelector("#to-do-list");
+
+// Listen for clicks on the list
+existingList.addEventListener("click", (event) => {
+	// If Edit button clicked, run edit function
+	if (event.target.classList.contains("edit-btn")) {
+		console.log("Editing…");
+	}
+	// Else, if Delete button clicked, print info to console and remove item
+	else if (event.target.classList.contains("del-btn")) {
+		const innerText =
+			event.target.parentNode.querySelector(".todo-item").innerText;
+		// const innerText = event.currentTarget.firstElementChild.firstElementChild.innerText;
+		console.log(`Deleting item "${innerText}"…`);
+		event.target.parentNode.remove();
+	}
 });
